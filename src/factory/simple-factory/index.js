af5d1916e549c29caf87b29c4ad974d07bfdae0c
tangweikun <819573105@qq.com>
2018-03-28 17:34:14 +0800

    :memo: Add comment for simple-factory

--- src/factory/simple-factory/index.js
+++ src/factory/simple-factory/index.js
@@ -1,17 +1,18 @@
+// 抽象产品角色 <Product>
 function Admin() {
   console.log('Admin created!')
 }
-
 function Customer() {
   console.log('Customer created')
 }
 
+// 工厂角色 <Factory>
 const UserFactory = {}
-
 UserFactory.createUser = function(type) {
   if (type === 'admin') return new Admin()
   if (type === 'customer') return new Customer()
 }
 
+// 具体产品角色 <ConcreteProduct>
 const admin = UserFactory.createUser('admin')
 const customer = UserFactory.createUser('customer')
