f5ad2a1dd00d306a584cb9ace113e45b717d9081
tangweikun <819573105@qq.com>
2018-04-26 13:26:59 +0800

    :Observer

--- src/behavioral/observer/Sample-7.js
+++ src/behavioral/observer/Sample-7.js
@@ -19,6 +19,27 @@ const event = {
   },
 }
 
+// 取消订阅
+event.remove = function(key, fn) {
+  let fns = this.clientList[key]
+
+  // 如果key对应的消息没有被人订阅，则直接返回
+  if (!fns) return false
+
+  // 如果没有传入具体的回调函数，表示需要取消key对应消息的所有订阅
+  if (!fn) {
+    fns && (fns.length = 0)
+  } else {
+    // 反向遍历订阅的回调函数列表
+    for (let l = fns.length - 1; l >= 0; l--) {
+      const _fn = fns[l]
+
+      // 删除订阅者的回调函数
+      if (_fn === fn) fns.splice(l, 1)
+    }
+  }
+}
+
 const installEvent = function(obj) {
   for (let i in event) {
     obj[i] = event[i]
@@ -30,14 +51,22 @@ const salesOffices = {}
 installEvent(salesOffices)
 
 // 小明订阅消息
-salesOffices.listen('squareMeter88', function(price) {
-  console.log('===小明订阅消息===>>价 格 = ' + price)
-})
+salesOffices.listen(
+  'squareMeter88',
+  (fn1 = function(price) {
+    console.log('===小明订阅消息===>>价 格 = ' + price)
+  }),
+)
 
 // 小红订阅消息
-salesOffices.listen('squareMeter100', function(price) {
-  console.log('===小红订阅消息===>>价 格 = ' + price)
-})
+salesOffices.listen(
+  'squareMeter100',
+  (fn2 = function(price) {
+    console.log('===小红订阅消息===>>价 格 = ' + price)
+  }),
+)
 
-salesOffices.trigger('squareMeter88', 2000000) // 输 出： 2000000
-salesOffices.trigger('squareMeter100', 3000000) // 输 出： 3000000
+salesOffices.remove('squareMeter88', fn1) // 删除小明的订阅
+
+salesOffices.trigger('squareMeter88', 2000000) // 输出：2000000
+salesOffices.trigger('squareMeter100', 3000000) // 输出：3000000
